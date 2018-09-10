a85b1a16e26cc013907324b8ac333dba50362ff8
sachinkodha <sachin@aylanetworks.com>
2018-09-10 00:34:31 -0700

    new code

--- prepration/src/solutions/ReconstructionOfTree.java
+++ prepration/src/solutions/ReconstructionOfTree.java
@@ -15,36 +15,39 @@ public class ReconstructionOfTree {
 			if(inorder[root_index]==root)
 				break;
 		}
-		System.out.println("root_index: "+root_index);
+		//System.out.println("root_index: "+root_index);
 		char[] left_preorder = new char[root_index];
 		for(int i=0; i<left_preorder.length; i++)
 		{
 			left_preorder[i]=preorder[1+i];
 		}
-		System.out.println("left_preorder: "+Arrays.toString(left_preorder));
+		//System.out.println("left_preorder: "+Arrays.toString(left_preorder));
 		
 		char[] right_preorder = new char[preorder.length-root_index-1];
 		for(int i=0; i<right_preorder.length; i++)
 		{
 			right_preorder[i]=preorder[1+i+root_index];
 		}
-		System.out.println("right_preorder: "+Arrays.toString(right_preorder));
+		//System.out.println("right_preorder: "+Arrays.toString(right_preorder));
 		
 		char[] left_inorder = new char[root_index];
 		for(int i=0; i<left_inorder.length; i++)
 		{
 			left_inorder[i]=inorder[i];
 		}
-		System.out.println("left_inorder: "+Arrays.toString(left_inorder));
+		//System.out.println("left_inorder: "+Arrays.toString(left_inorder));
 		
 		char[] right_inorder = new char[inorder.length-root_index-1];
 		for(int i=0; i<right_inorder.length; i++)
 		{
 			right_inorder[i]=inorder[1+i+root_index];
 		}
-		System.out.println("right_inorder: "+Arrays.toString(right_inorder));
-		int root_left = reconstruct_tree(left_preorder, left_inorder);
-		int root_right = reconstruct_tree(right_preorder, right_inorder);
+		//System.out.println("right_inorder: "+Arrays.toString(right_inorder));
+		char root_left = reconstruct_tree(left_preorder, left_inorder);
+		char root_right = reconstruct_tree(right_preorder, right_inorder);
+		
+		System.out.print(" \nleft_root: "+root_left);
+		System.out.print(" \nright_root: "+root_right);
 		
 		return root;
 		
@@ -58,7 +61,7 @@ public class ReconstructionOfTree {
 		System.out.println("Preorder array is: "+Arrays.toString(preorder));
 		System.out.println("Inorder array is: "+Arrays.toString(inorder));
 		
-		System.out.println("Reconstructed Tree is: "+reconstruct_tree(preorder, inorder));
+		System.out.println("\nReconstructed Tree is: "+reconstruct_tree(preorder, inorder));
 
 	}
 
